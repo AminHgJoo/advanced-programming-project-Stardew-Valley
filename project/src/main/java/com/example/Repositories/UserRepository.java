711d8d33d884d687c0dd2a70c24ad912effb3035
soroosh hamedifar <hamedifarsoroosh@gmail.com>
2025-04-14 13:45:00 +0330

    adding repositories and validations

---
+++ project/src/main/java/com/example/Repositories/UserRepository.java
@@ -0,0 +1,24 @@
+package com.example.Repositories;
+
+import com.example.models.User;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class UserRepository {
+    public static User findUserById(int id) {
+        return null;
+    }
+    public static User findUserByUsername(String username) {
+        return null;
+    }
+    public static void saveUser(User user) {
+
+    }
+    public static ArrayList<User> findAllUsers() {
+        return null;
+    }
+    public static void removeUser(User user) {
+
+    }
+}
---
+++ project/src/main/java/com/example/utilities/Validation.java
@@ -0,0 +1,13 @@
+package com.example.utilities;
+
+public class Validation {
+    public static boolean validatePassword(String password) {
+        return false;
+    }
+    public static boolean validateUsername(String username) {
+        return false;
+    }
+    public static boolean validateEmail(String email) {
+        return false;
+    }
+}
