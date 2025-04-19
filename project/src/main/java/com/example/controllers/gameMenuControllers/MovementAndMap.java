46f97085e8347ac5c02230f33f09397eaaad5b6c
ASUS <pouyaastarakimain@gmail.com>
2025-04-19 14:59:54 +0330

    energy set and get

--- project/src/main/java/com/example/controllers/gameMenuControllers/MovementAndMap.java
+++ project/src/main/java/com/example/controllers/gameMenuControllers/MovementAndMap.java
@@ -1,8 +1,11 @@
 package com.example.controllers.gameMenuControllers;
 
 import com.example.controllers.Controller;
+import com.example.models.App;
+import com.example.models.Game;
 import com.example.models.IO.Request;
 import com.example.models.IO.Response;
+import com.example.models.User;
 
 public class MovementAndMap extends Controller {
     public static Response handleWalking(Request request) {
@@ -21,17 +24,29 @@ public class MovementAndMap extends Controller {
     }
 
     public static Response handleShowEnergy(Request request) {
-        return null;
-
+        User user = App.getLoggedInUser();
+        Game game = user.getCurrentGame();
+        double energy = game.getCurrentPlayer().getEnergy();
+        if(energy == Double.POSITIVE_INFINITY) {
+            return new Response(true, "infinity");
+        }
+        String energyString = String.valueOf((int)energy);
+        return new Response(true, energyString);
     }
 
     public static Response handleSetEnergy(Request request) {
-        return null;
-
+        String energy = request.body.get("value");
+        User user = App.getLoggedInUser();
+        Game game = user.getCurrentGame();
+        game.getCurrentPlayer().setEnergy(Double.parseDouble(energy));
+        return new Response(true, "energy successfully set to " + energy);
     }
 
     public static Response handleUnlimitedEnergy(Request request) {
-        return null;
+        User user = App.getLoggedInUser();
+        Game game = user.getCurrentGame();
+        game.getCurrentPlayer().setEnergy(Double.POSITIVE_INFINITY);
+        return new Response(true, "energy successfully set to infinity");
 
     }
 }
