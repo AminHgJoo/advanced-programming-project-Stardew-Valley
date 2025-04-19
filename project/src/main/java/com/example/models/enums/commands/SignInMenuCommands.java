b50f973efcbccbabfec03bb292dde9d524b69142
soroosh hamedifar <hamedifarsoroosh@gmail.com>
2025-04-19 08:20:42 +0330

    fix login controller

--- project/src/main/java/com/example/models/enums/commands/SignInMenuCommands.java
+++ project/src/main/java/com/example/models/enums/commands/SignInMenuCommands.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 public enum SignInMenuCommands implements Command {
     REGISTER("^register\\s+-u\\s+(?<username>.+?)\\s+-p\\s+(?<password>.+?)\\s+(?<passwordConfirm>.+?)\\s+-n\\s+(?<nickname>.+?)\\s+-e\\s+(?<email>.+?)\\s+-g\\s+(?<gender>.+)$"),
     PICK_QUESTION("^pick\\s+question\\s+-q\\s+(?<questionNumber>\\d+)\\s+-a\\s+(?<answer>.+?)\\s+-c\\s+(?<answerConfirm>.+)$"),
-    LOGIN("^login\\s+-u\\s+(?<username>.+?)\\s+-p\\s+(?<password>.+?)(\\s+(?<loginFlag>–stay-logged-in))?$"),
+    LOGIN("^login\\s+-u\\s+(?<username>.+?)\\s+-p\\s+(?<password>\\S+)\\s*(?<loginFlag>\\S+)?$"),
     FORGET("^forget\\s+password\\s+-u\\s+(?<username>.+)$"),
     ANSWER("^answer\\s+-a\\s+(?<answer>.+)$"),
     LIST_QUESTIONS("^list\\s+questions$"),
@@ -33,6 +33,10 @@ public enum SignInMenuCommands implements Command {
     public String getGroup(String input, String group) {
         Matcher matcher = getMatcher(input);
         matcher.find();
-        return matcher.group(group).trim();
+        String value = matcher.group(group);
+        if(group.equals("loginFlag")) {
+            if(!value.equals("-stay-logged-in")) value = null;
+        }
+        return  value != null ? value.trim() : null;
     }
 }
