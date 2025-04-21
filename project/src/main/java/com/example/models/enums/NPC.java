0ac1dc7d147589c6b7740144a04a1738a43cb82b
ASUS <pouyaastarakimain@gmail.com>
2025-04-21 12:57:58 +0330

    npc enum added

--- project/src/main/java/com/example/models/enums/NPC.java
+++ project/src/main/java/com/example/models/enums/NPC.java
@@ -11,7 +11,14 @@ public enum NPC  {
     ABIGAIL("Abigail", new Item[]{}, new NPCRequest(new Item[]{}), new NPCReward(500, new Item[]{})),
     HARVEY("Harvey", new Item[]{}, new NPCRequest(new Item[]{}), new NPCReward(750, new Item[]{})),
     LYA("Lya", new Item[]{}, new NPCRequest(new Item[]{}), new NPCReward(500, new Item[]{})),
-    ROBIN("Robin", new Item[]{}, new NPCRequest(new Item[]{}), new NPCReward(25000, new Item[]{}));
+    ROBIN("Robin", new Item[]{}, new NPCRequest(new Item[]{}), new NPCReward(25000, new Item[]{})),
+    CLINT("Clint", null, null, null),
+    MORRIS("Morris", null, null, null),
+    PIERRE("Pierre", null, null, null),
+    WILLY("Willy", null, null, null),
+    MARNIE("Marnie", null, null, null),
+    GUS("Gus", null, null, null),
+    ;
     public final String name;
     public final Item[] items;
     public final NPCRequest request;
