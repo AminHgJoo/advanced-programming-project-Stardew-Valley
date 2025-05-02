a88c2cf9c7c314f96e8dd2fadec24e87af243027
soroosh hamedifar <hamedifarsoroosh@gmail.com>
2025-05-02 12:46:24 +0330

    initialize npc

--- project/src/main/java/com/example/models/NPCModels/NPC.java
+++ project/src/main/java/com/example/models/NPCModels/NPC.java
@@ -1,6 +1,9 @@
 package com.example.models.NPCModels;
 
 import com.example.models.Quest;
+import com.example.models.enums.types.itemTypes.*;
+import com.example.models.items.Food;
+import com.example.models.items.ForagingMineral;
 import com.example.models.items.Item;
 import com.example.models.mapModels.Coordinate;
 import dev.morphia.annotations.Embedded;
@@ -10,7 +13,7 @@ import java.util.ArrayList;
 @Embedded
 public class NPC {
     private String name;
-    private ArrayList<Item> favoriteItems = new ArrayList<>();
+    private ArrayList<ItemType> favoriteItems = new ArrayList<>();
     private ArrayList<NPCRequest> npcRequests = new ArrayList<>();
     private ArrayList<NPCReward> rewards = new ArrayList<>();
     private Coordinate coordinate;
@@ -24,13 +27,15 @@ public class NPC {
     public NPC(String name, Coordinate coordinate) {
         this.name = name;
         this.coordinate = coordinate;
+        initializeNpcQuest(name);
+        initializeNpcFavorites(name);
     }
 
     public String getName() {
         return name;
     }
 
-    public ArrayList<Item> getFavoriteItems() {
+    public ArrayList<ItemType> getFavoriteItems() {
         return favoriteItems;
     }
 
@@ -60,5 +65,53 @@ public class NPC {
 
     public ArrayList<Quest> getQuests() {
         return quests;
+    }
+
+    public void initializeNpcQuest(String name){
+        if(name.equals("Sebastian")){
+            quests.add(new Quest(ForagingMineralsType.IRON_ORE , 50));
+            quests.add(new Quest(FoodTypes.PUMPKIN_PIE, 1));
+            quests.add(new Quest(ForagingMineralsType.STONE , 150));
+        }else if(name.equals("Abigale")){
+            quests.add(new Quest(MiscType.GOLD_BAR, 1));
+            quests.add(new Quest(FoodTypes.PUMPKIN , 1));
+            quests.add(new Quest(FoodTypes.WHEAT , 50));
+        }else if(name.equals("Harvey")){
+            quests.add(new Quest(CropSeedsType.BLUE_JAZZ, 12));
+            quests.add(new Quest(FishType.SALMON, 1));
+            quests.add(new Quest(FoodTypes.WINE, 1));
+        }else if(name.equals("Lia")){
+            quests.add(new Quest(MiscType.WOOD , 10));
+            quests.add(new Quest(FishType.SALMON, 1));
+            quests.add(new Quest(MiscType.WOOD , 200));
+        }else {
+            quests.add(new Quest(MiscType.WOOD , 80));
+            quests.add(new Quest(MiscType.IRON_BAR , 10));
+            quests.add(new Quest(MiscType.WOOD , 1000));
+        }
+    }
+
+    public void initializeNpcFavorites(String name){
+        if(name.equals("Sebastian")){
+            favoriteItems.add(MiscType.WOOL);
+            favoriteItems.add(FoodTypes.PUMPKIN_PIE);
+            favoriteItems.add(FoodTypes.PIZZA);
+        }else if(name.equals("Abigale")){
+            favoriteItems.add(ForagingMineralsType.STONE);
+            favoriteItems.add(ForagingMineralsType.IRON_ORE);
+            favoriteItems.add(FoodTypes.COFFEE);
+        }else if(name.equals("Harvey")){
+            favoriteItems.add(FoodTypes.COFFEE);
+            favoriteItems.add(FoodTypes.PICKLES);
+            favoriteItems.add(FoodTypes.WINE);
+        }else if(name.equals("Lia")){
+            favoriteItems.add(FoodTypes.SALAD);
+            favoriteItems.add(FoodTypes.GRAPE);
+            favoriteItems.add(FoodTypes.WINE);
+        }else {
+            favoriteItems.add(FoodTypes.SPAGHETTI);
+            favoriteItems.add(MiscType.WOOD);
+            favoriteItems.add(MiscType.IRON_BAR);
+        }
     }
 }
--- project/src/main/java/com/example/models/Quest.java
+++ project/src/main/java/com/example/models/Quest.java
@@ -1,8 +1,40 @@
 package com.example.models;
 
+import com.example.models.enums.types.itemTypes.ItemType;
+import com.example.models.items.Item;
 import dev.morphia.annotations.Embedded;
 
 @Embedded
 public class Quest {
+    private int count;
+    private ItemType item;
+    private boolean completed = false;
 
+    public Quest() {
+
+    }
+    public Quest(ItemType item , int count) {
+        this.item = item;
+        this.count = count;
+    }
+    public int getCount() {
+        return count;
+    }
+    public void setCount(int count) {
+        this.count = count;
+    }
+    public ItemType getItem() {
+        return item;
+    }
+    public void setItem(ItemType item) {
+        this.item = item;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
 }
