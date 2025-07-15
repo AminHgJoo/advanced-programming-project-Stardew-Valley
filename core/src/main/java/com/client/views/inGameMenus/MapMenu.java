5773986b2265b4b5a015ab4bbd0f2320da3cf3ad
ASUS <pouyaastarakimain@gmail.com>
2025-07-15 13:11:00 +0330

    inventory menu template

---
+++ core/src/main/java/com/client/views/inGameMenus/MapMenu.java
@@ -0,0 +1,146 @@
+package com.client.views.inGameMenus;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.client.GameMain;
+import com.client.utils.AssetManager;
+import com.client.utils.MyScreen;
+
+public class MapMenu implements MyScreen, InputProcessor {
+    private GameMain gameMain;
+    private MyScreen farmScreen;
+    private SpriteBatch batch;
+    private Texture backgroundTexture;
+    private BitmapFont titleFont;
+    private GlyphLayout layout;
+    private Stage stage;
+    private final Skin skin;
+
+    public MapMenu(GameMain gameMain, MyScreen farmScreen) {
+        this.gameMain = gameMain;
+        this.farmScreen = farmScreen;
+        this.batch = new SpriteBatch();
+        backgroundTexture = AssetManager.getImage("profileBackground");
+        this.skin = AssetManager.getSkin();
+        titleFont = AssetManager.getFont();
+        titleFont.getData().setScale(3f);
+        titleFont.setColor(Color.WHITE);
+        this.layout = new GlyphLayout();
+    }
+
+    @Override
+    public boolean keyDown(int i) {
+        if (i == Input.Keys.ESCAPE) {
+            gameMain.setScreen(farmScreen);
+            this.dispose();
+        }
+        else if(i == Input.Keys.LEFT) {
+            gameMain.setScreen(new SocialMenu(gameMain, farmScreen));
+        }
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int i) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char c) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int i, int i1, int i2, int i3) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int i, int i1, int i2, int i3) {
+        return false;
+    }
+
+    @Override
+    public boolean touchCancelled(int i, int i1, int i2, int i3) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int i, int i1, int i2) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int i, int i1) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(float v, float v1) {
+        return false;
+    }
+
+    @Override
+    public void socketMessage(String message) {
+
+    }
+
+    @Override
+    public void show() {
+        Gdx.input.setInputProcessor(this);
+    }
+
+    @Override
+    public void render(float v) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+        batch.draw(backgroundTexture, 0, 0);
+
+        String title = "Map";
+        layout.setText(titleFont, title);
+
+        float xAsghar = Gdx.graphics.getWidth() / 2f - layout.width / 2f;
+        float yAsghar = Gdx.graphics.getHeight() - 50f;
+        titleFont.draw(batch, layout, xAsghar, yAsghar);
+        batch.end();
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
