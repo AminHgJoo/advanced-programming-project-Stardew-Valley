ab2c61cf4ed25059687d19c31d0cf742fa9953f2
AminHgJoo <mahaghjoo63@gmail.com>
2025-07-20 13:24:00 +0330

    Successful Tof.

--- core/src/main/java/com/client/views/inGameMenus/CraftingMenu.java
+++ core/src/main/java/com/client/views/inGameMenus/CraftingMenu.java
@@ -1,14 +1,13 @@
 package com.client.views.inGameMenus;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import com.client.ClientApp;
 import com.client.GameMain;
 import com.client.utils.AssetManager;
 import com.client.utils.MyScreen;
@@ -23,6 +22,9 @@ public class CraftingMenu implements MyScreen {
     private Stage stage;
     private final Texture background;
 
+    private Label selectedRecipeLabel;
+    private CraftingRecipes selectedRecipe;
+
     public CraftingMenu(GameMain gameMain, FarmMenu farmMenu) {
         this.gameMain = gameMain;
         this.farmMenu = farmMenu;
@@ -35,6 +37,8 @@ public class CraftingMenu implements MyScreen {
 
     private void initializeStage() {
         stage = new Stage(new ScreenViewport());
+
+        selectedRecipeLabel = new Label("Selected Recipe Will Show Up Here!", skin);
 
         Image bg = new Image(background);
         stage.addActor(bg);
@@ -50,6 +54,7 @@ public class CraftingMenu implements MyScreen {
 
         //TODO: Replace with ClientApp.currentPlayer
         Player player = new Player();
+        player.getUnlockedCraftingRecipes().add(CraftingRecipes.FURNACE);
 
         CraftingRecipes[] allRecipes = CraftingRecipes.values();
         CraftingRecipes[][] descriptions = new CraftingRecipes[4][5];
@@ -64,28 +69,51 @@ public class CraftingMenu implements MyScreen {
                 CraftingRecipes recipes = descriptions[row][col];
 
                 Image img = new Image(AssetManager.getImage(AssetManager.generateKeyFromFileName(recipes.name)));
-                img.addListener(new TextTooltip(recipes.name + "\n" + recipes.description + "\n" + recipes.listIngredients(), skin));
 
                 if (!player.getUnlockedCraftingRecipes().contains(recipes)) {
                     img.setColor(0.6f, 0.6f, 0.6f, 0.6f);
-                } else {
-                    img.addListener(new ChangeListener() {
-                        @Override
-                        public void changed(ChangeEvent event, Actor actor) {
-
-                        }
-                    });
                 }
+
+                var listener = new TextTooltip(recipes.name + "\n" + recipes.description + "\n" + recipes.listIngredients(), skin) {
+                    @Override
+                    public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                        boolean res = super.touchDown(event, x, y, pointer, button);
+                        if (player.getUnlockedCraftingRecipes().contains(recipes)) {
+                            selectedRecipe = recipes;
+                            selectedRecipeLabel.setText("Selected Recipe: " + selectedRecipe.name);
+                        }
+                        return res;
+                    }
+                };
+                img.addListener(listener);
 
                 leftTable.add(img).pad(10);
             }
             leftTable.row();
         }
 
-// Add your widgets to leftTable and rightTable here
-// e.g. leftTable.add(button1).row(); rightTable.add(label).row();
+        TextButton backButton = new TextButton("Back", skin);
+        backButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                dispose();
+                gameMain.setScreen(farmMenu);
+            }
+        });
 
-// Split evenly: expandX + fillX on each cell
+        TextButton craftButton = new TextButton("Craft", skin);
+        craftButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                //TODO
+            }
+        });
+
+        rightTable.add(selectedRecipeLabel).pad(10).row();
+        rightTable.row().row();
+        rightTable.add(craftButton).pad(10).row();
+        rightTable.add(backButton).pad(10).row();
+
         root.add(leftTable).expandX().fillX();
         root.add(rightTable).expandX().fillX();
 
